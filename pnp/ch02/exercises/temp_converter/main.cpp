46ef9fffc51cd22ed54412034873b4fcc4d21795
Ronak Parekh <rontrade@outlook.com>
2026-04-28 19:11:27 -0400

    Updated temperature

--- pnp/ch02/exercises/temp_converter/main.cpp
+++ pnp/ch02/exercises/temp_converter/main.cpp
@@ -2,12 +2,33 @@
 
 int main() {
 
-  double fTemp{};
-  std::cout << "Enter a temperature in farenheit: ";
-  std::cin >> fTemp;
+  double temperature{};
+  int choice{};
 
-  double cTemp{(fTemp - 32) * (5.0 / 9.0)};
-  std::cout << "Temperature in celsius: " << cTemp << '\n';
+  std::cout << "Choose a conversion:\n"
+            << "1 -> Fahrenheit to Celsius\n"
+            << "2 -> Celsius to Fahrenheit\n"
+            << "Enter choice: ";
+
+  std::cin >> choice;
+
+  if (choice == 1) {
+    std::cout << "Enter temperature in Fahrenheit: ";
+    std::cin >> temperature;
+
+    std::cout << "Temperature in Celsius: " << (temperature - 32.0) * 5.0 / 9.0
+              << '\n';
+
+  } else if (choice == 2) {
+    std::cout << "Enter temperature in Celsius: ";
+    std::cin >> temperature;
+
+    std::cout << "Temperature in Fahrenheit: "
+              << (temperature * 9.0 / 5.0) + 32.0 << '\n';
+
+  } else {
+    std::cout << "Invalid choice\n";
+  }
 
   return 0;
 }
