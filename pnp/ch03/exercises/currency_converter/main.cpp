4c28a646801ce1a283021f78d38a1a845993e360
Ronak Parekh <rontrade@outlook.com>
2026-04-30 16:34:57 -0400

    Second attempt

--- pnp/ch03/exercises/currency_converter/main.cpp
+++ pnp/ch03/exercises/currency_converter/main.cpp
@@ -1,14 +1,16 @@
+#include <cctype>
 #include <iostream>
 
 int main() {
 
-  constexpr double y_dollars = 1.24;
-  constexpr double k_dollars = 1.30;
-  constexpr double p_dollars = 2.53;
+  // Conversions
+  constexpr double YEN_TO_USD{0.0065};
+  constexpr double KRONE_TO_USD{0.09};
+  constexpr double POUND_TO_USD{1.25};
 
   // User selection
-  double amount;
-  char choice;
+  double amount{};
+  char choice{};
 
   // Ask user for conversion choice
   std::cout << "Pick which currency to convert into dollars (p, k, y): ";
@@ -16,15 +18,18 @@ int main() {
   std::cout << "Enter the amount: $";
   std::cin >> amount;
 
+  // Convert to lowercase
+  choice = std::tolower(choice);
+
   // Print the converted amount
   if (choice == 'p') {
-    std::cout << "Pounds converted into dollars: $" << amount * p_dollars
+    std::cout << "Pounds converted into dollars: $" << amount * POUND_TO_USD
               << '\n';
   } else if (choice == 'k') {
-    std::cout << "Krones convtered into dollars: $" << amount * k_dollars
+    std::cout << "Krones converted into dollars: $" << amount * KRONE_TO_USD
               << '\n';
   } else if (choice == 'y') {
-    std::cout << "Yen converted into dollars: $" << amount * y_dollars << '\n';
+    std::cout << "Yen converted into dollars: $" << amount * YEN_TO_USD << '\n';
   } else {
     std::cout << "BAD INPUT!" << '\n';
   }
